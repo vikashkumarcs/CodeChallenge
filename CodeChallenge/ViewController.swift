b0f71660b7627346edb3f4145567693afd60198b
Vikash Kumar <vikash.kumar2-external@tcs.com>
2020-08-07 21:34:01 +0530

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Vikash on 07/08/20.
+//  Copyright © 2020 Vikash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
